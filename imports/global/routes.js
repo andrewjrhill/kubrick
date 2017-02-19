a0809b5f4a29298d4ab48b1462ebb825b781386d
Andrew Hill <andrewjrhill@gmail.com>
2017-02-19 22:27:29 +0200

    Redirect to movies list view on 404

--- imports/global/routes.js
+++ imports/global/routes.js
@@ -1,8 +1,17 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
-// Home Route
+// Redirect to the movies list view route when hitting
+// the applications root route.
 FlowRouter.route('/', {
   triggersEnter: [() => {
     FlowRouter.redirect('/movies');
   }],
 });
+
+// Redirect to the movies list view route when hitting
+// a URL the application does not recognise
+FlowRouter.notFound = {
+  action: () => {
+    FlowRouter.redirect('/movies');
+  },
+};
