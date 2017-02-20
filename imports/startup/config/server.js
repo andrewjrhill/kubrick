a4dee9bdcbe31e2029821ff601d2a7da0f577348
Andrew Hill <andrewjrhill@gmail.com>
2017-02-20 13:54:59 +0200

    Startup documentation

--- imports/startup/config/server.js
+++ imports/startup/config/server.js
@@ -1,8 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 import { BrowserPolicy } from 'meteor/browser-policy-common';
 
-// On startup, server package configuration
+// On startup, server package configuration.
 Meteor.startup(() => {
+  // Setup browser policies for added security.
+  BrowserPolicy.framing.disallow();
+  BrowserPolicy.content.disallowEval();
+  BrowserPolicy.content.allowFontDataUrl();
+  BrowserPolicy.content.allowFontDataUrl();
+  BrowserPolicy.content.allowInlineStyles();
+
   // A list of URLs to be trusted by the app to prevent injections/iFraming.
   const trustedURLs = [
     'fonts.googleapis.com',
@@ -10,12 +17,7 @@ Meteor.startup(() => {
     'image.tmdb.org',
   ];
 
-  BrowserPolicy.framing.disallow();
-  BrowserPolicy.content.disallowEval();
-  BrowserPolicy.content.allowFontDataUrl();
-  BrowserPolicy.content.allowFontDataUrl();
-  BrowserPolicy.content.allowInlineStyles();
-
+  // Loop over list of trusted URLs to accept content from.
   if (!trustedURLs || !trustedURLs.count) {
     trustedURLs.map((origin) => BrowserPolicy.content.allowOriginForAll(origin));
   }
